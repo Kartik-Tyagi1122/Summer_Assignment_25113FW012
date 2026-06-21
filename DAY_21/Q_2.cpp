2b250d0da440c866e948df1416498bd991b13512
Kartik-Tyagi1122 <tyagikartik317@gmail.com>
2026-06-21 21:30:11 +0530

    day 21 uploaded

---
+++ DAY_21/Q_2.cpp
@@ -0,0 +1,19 @@
+//reverse strinf
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string : ";
+    getline(cin, str);
+
+    for(int i = str.length() - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+
+    cout << endl;
+
+    return 0;
+}
